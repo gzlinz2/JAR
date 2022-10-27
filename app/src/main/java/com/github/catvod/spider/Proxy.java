5a695f38f25dea0a1f5eb89bf4f7b408ebc96393
gzlinz2 <107169210+gzlinz2@users.noreply.github.com>
2022-10-27 15:09:44 +0800

    Add files via upload

--- app/src/main/java/com/github/catvod/spider/Proxy.java
+++ app/src/main/java/com/github/catvod/spider/Proxy.java
@@ -1,45 +1,69 @@
+//
+// Decompiled by Jadx - 589ms
+//
 package com.github.catvod.spider;
 
+import android.util.Base64;
 import com.github.catvod.crawler.Spider;
 import com.github.catvod.crawler.SpiderDebug;
-import com.github.catvod.net.OkHttpUtil;
-
+import com.github.catvod.parser.MixDemo;
+import com.github.catvod.parser.MixWeb;
+import com.github.catvod.spider.merge.Vo;
+import com.github.catvod.spider.merge.mI;
 import java.io.ByteArrayInputStream;
-import java.nio.charset.StandardCharsets;
 import java.util.Map;
-import java.util.Objects;
 
 public class Proxy extends Spider {
+    public static int I = -1;
 
-    private static int port = -1;
-
-    public static Object[] proxy(Map<String, String> params) {
-        switch (Objects.requireNonNull(params.get("do"))) {
-            case "ck":
-                return new Object[]{200, "text/plain; charset=utf-8", new ByteArrayInputStream("ok".getBytes(StandardCharsets.UTF_8))};
-            case "ali":
-                return Ali.vod(params);
-            default:
-                return null;
-        }
-    }
-
-    static void adjustPort() {
-        if (Proxy.port > 0) return;
-        int port = 9978;
-        while (port < 10000) {
-            String resp = OkHttpUtil.string("http://127.0.0.1:" + port + "/proxy?do=ck", null);
-            if (resp.equals("ok")) {
-                SpiderDebug.log("Found local server port " + port);
-                Proxy.port = port;
-                break;
+    static void I() {
+        if (I <= 0) {
+            for (int i = 9978; i < 10000; i++) {
+                if (mI.gR("http://127.0.0.1:" + i + "/proxy?do=ck", (Map) null).equals("ok")) {
+                    SpiderDebug.log("Found local server port " + i);
+                    I = i;
+                    return;
+                }
             }
-            port++;
         }
     }
 
-    public static String getUrl() {
-        adjustPort();
-        return "http://127.0.0.1:" + port + "/proxy";
+    public static String localProxyUrl() {
+        I();
+        return "http://127.0.0.1:" + I + "/proxy";
+    }
+
+    public static Object[] proxy(Map<String, String> map) {
+        try {
+            String str = map.get("do");
+            if (str.equals("live")) {
+                if (!map.get("type").equals("txt")) {
+                    return null;
+                }
+                String str2 = map.get("ext");
+                if (!str2.startsWith("http")) {
+                    str2 = new String(Base64.decode(str2, 10), "UTF-8");
+                }
+                return Vo.V7(str2);
+            } else if (str.equals("MixDemo")) {
+                return MixDemo.loadHtml(map.get("flag"), map.get("url"));
+            } else {
+                if (str.equals("MixWeb")) {
+                    return MixWeb.loadHtml(map.get("flag"), map.get("url"));
+                }
+                if (str.equals("ck")) {
+                    return new Object[]{200, "text/plain; charset=utf-8", new ByteArrayInputStream("ok".getBytes("UTF-8"))};
+                }
+                if (str.equals("push")) {
+                    return PushAgent.vod(map);
+                }
+                if (str.equals("xBPQ")) {
+                    return xBPQ.loadPic(map);
+                }
+                return null;
+            }
+        } catch (Throwable unused) {
+            return null;
+        }
     }
 }
